e7ee9c5aa019df300364803115ab65778b5c18e2
Victoria Martini <victoria.martini@etu.u-bordeaux.fr>
2025-12-05 15:23:20 +0100

    Création du dossier billetterie service

---
+++ billeterie-service/src/main/java/com/billeterie/billeterie_service/BilleterieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.billeterie.billeterie_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleterieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleterieServiceApplication.class, args);
+	}
+
+}
---
+++ billeterie-service/src/test/java/com/billeterie/billeterie_service/BilleterieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billeterie.billeterie_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleterieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
